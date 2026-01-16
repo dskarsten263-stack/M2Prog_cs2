913b824e0e973138663d116813a1dae33b701930
Daimien Karsten <36749@ma-web.nl>
2026-01-16 12:02:27 +0100

    0

---
+++ 06 fileio/00/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _00; 
+using System;
+using System.IO;
+ 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+        string[] lines = File.ReadAllLines("quiz.txt");
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+        string[] lines2 = File.ReadAllLines("nos.txt");
+        foreach (string line in lines2)
+        {
+            Console.WriteLine(line);
+        }
+    }
+ 
+    static void Run()
+    {
+        Console.WriteLine("Project is running!");
+        Console.WriteLine(Environment.CurrentDirectory);
+    }
+}
